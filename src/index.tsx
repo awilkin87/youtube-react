1f62c9ffd19e482d8b6a1219c965972ed1e6a989
Alan Wilkinson <Alan.Wilkinson@q-free.com>
2020-12-22 23:24:04 -0500

    Add header nav bar

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
+import 'semantic-ui-css/semantic.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
