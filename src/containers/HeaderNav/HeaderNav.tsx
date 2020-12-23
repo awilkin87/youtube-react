31b3d7ebe5e7ad70510d5e6568180e608c315722
Alan Wilkinson <Alan.Wilkinson@q-free.com>
2020-12-23 01:37:57 -0500

    Visual tweaks outside tutorial for personal prefs

--- src/containers/HeaderNav/HeaderNav.tsx
+++ src/containers/HeaderNav/HeaderNav.tsx
@@ -5,35 +5,33 @@ import logo from '../../assets/images/logo.jpg';
 export default function HeaderNav() {
   return (
     <Menu borderless className='top-menu' fixed='top'>
-      <Menu.Item header className='logo'>
+      <Menu.Item header className='left'>
         <Image src={logo} size='tiny' />
       </Menu.Item>
-      <Menu.Menu className='nav-container'>
-        <Menu.Item className='search-input'>
-          <Form>
-            <Form.Field>
-              <Input placeholder='Search' size='small' action='Go' />
-            </Form.Field>
-          </Form>
+      <Menu.Item className='center'>
+        <Form className='search'>
+          <Form.Field>
+            <Input placeholder='Search' size='small' action='Go' />
+          </Form.Field>
+        </Form>
+      </Menu.Item>
+      <Menu.Item position='right'>
+        <Menu.Item>
+          <Icon className='header-icon' name='video camera' size='large' />
         </Menu.Item>
-        <Menu.Menu position='right'>
-          <Menu.Item>
-            <Icon className='header-icon' name='video camera' size='large' />
-          </Menu.Item>
-          <Menu.Item>
-            <Icon className='header-icon' name='grid layout' size='large' />
-          </Menu.Item>
-          <Menu.Item>
-            <Icon className='header-icon' name='chat' size='large' />
-          </Menu.Item>
-          <Menu.Item>
-            <Icon className='header-icon' name='alarm' size='large' />
-          </Menu.Item>
-          <Menu.Item name='avatar'>
-            <Image src='http://via.placeholder.com/80x80' avatar />
-          </Menu.Item>
-        </Menu.Menu>
-      </Menu.Menu>
+        <Menu.Item>
+          <Icon className='header-icon' name='grid layout' size='large' />
+        </Menu.Item>
+        <Menu.Item>
+          <Icon className='header-icon' name='chat' size='large' />
+        </Menu.Item>
+        <Menu.Item>
+          <Icon className='header-icon' name='alarm' size='large' />
+        </Menu.Item>
+        <Menu.Item name='avatar'>
+          <Image src='http://via.placeholder.com/80x80' avatar />
+        </Menu.Item>
+      </Menu.Item>
     </Menu>
   );
 }
