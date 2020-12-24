77cd6a0918b40b994571940664166abc18290717
Alan Wilkinson <Alan.Wilkinson@q-free.com>
2020-12-24 09:50:04 -0500

    Add subsections to sidebar

--- src/containers/SideBar/SideBar.tsx
+++ src/containers/SideBar/SideBar.tsx
@@ -1,5 +1,6 @@
-import { Menu } from 'semantic-ui-react';
+import { Divider, Menu } from 'semantic-ui-react';
 import './SideBar.scss';
+import SideBarHeader from './SideBarHeader';
 import SideBarItem from './SideBarItem';
 
 export default function SideBar() {
@@ -8,13 +9,19 @@ export default function SideBar() {
       <SideBarItem highlight label='Home' icon='home' />
       <SideBarItem label='Trending' icon='fire' />
       <SideBarItem label='Followers' icon='users' />
+      <Divider />
+      <SideBarHeader title='Library' />
       <SideBarItem label='History' icon='history' />
       <SideBarItem label='Watch Later' icon='clock' />
       <SideBarItem label='Liked Videos' icon='thumbs up' />
+      <Divider />
+      <SideBarHeader title='More from YouTube' />
       <SideBarItem label='Movies and Shows' icon='film' />
+      <Divider />
       <SideBarItem label='Report History' icon='flag' />
       <SideBarItem label='Help' icon='help circle' />
       <SideBarItem label='Send Feedback' icon='comment' />
+      <Divider />
     </Menu>
   );
 }
---
+++ src/containers/SideBar/SideBarHeader.tsx
@@ -0,0 +1,15 @@
+import { Menu } from 'semantic-ui-react';
+import './SideBarHeader.scss';
+
+export interface SideBarHeaderProps {
+  title: string
+}
+
+export default function SideBarHeader(props: SideBarHeaderProps) {
+  const heading = props.title || '';
+  return (
+    <Menu.Item>
+      <Menu.Header className='sidebar-header'>{heading.toUpperCase()}</Menu.Header>
+    </Menu.Item>
+  );
+}
