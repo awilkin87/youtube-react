233d8e4ed49846f21dc119d5b05d92ce4b4ca5ff
Alan Wilkinson <Alan.Wilkinson@q-free.com>
2020-12-28 18:23:47 -0500

    Add divider to VideoInfoBox

--- src/components/VideoInfoBox/VideoInfoBox.tsx
+++ src/components/VideoInfoBox/VideoInfoBox.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, Image } from 'semantic-ui-react';
+import { Button, Divider, Image } from 'semantic-ui-react';
 import styles from './VideoInfoBox.module.scss';
 
 export interface VideoInfoBoxProps {
@@ -10,27 +10,30 @@ const VideoInfoBox: React.FunctionComponent<Readonly<VideoInfoBoxProps>> = funct
   const [collapsed, setCollapsed] = useState(true);
 
   return (
-    <div className={styles.container}>
-      <Image className={styles.channelImage} src='http://via.placeholder.com/48x48' circular />
-      <div className={styles.videoInfo}>
-        <div className={styles.channelName}>Channel Name</div>
-        <div className={styles.subscriberCount}>80K Subscribers</div>
-      </div>
-      <Button className={styles.subscribeButton} color='red'>Subscribe</Button>
-      <div className={styles.description}>
-        <div className={collapsed ? styles.collapsed : styles.expanded}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id tempor massa. Duis in
-            suscipit urna. Nunc vel rhoncus lectus. Fusce facilisis mauris at libero mattis porta.
-            Donec elementum orci ac consectetur venenatis. In scelerisque, mauris efficitur volutpat
-            pellentesque, odio mi congue nisl, sit amet accumsan ipsum nibh at odio.
-        </p>
+    <>
+      <div className={styles.container}>
+        <Image className={styles.channelImage} src='http://via.placeholder.com/48x48' circular />
+        <div className={styles.videoInfo}>
+          <div className={styles.channelName}>Channel Name</div>
+          <div className={styles.subscriberCount}>80K Subscribers</div>
         </div>
-        <Button compact onClick={() => setCollapsed(!collapsed)}>
-          Show {collapsed ? 'More' : 'Less'}
-        </Button>
+        <Button className={styles.subscribeButton} color='red'>Subscribe</Button>
+        <div className={styles.description}>
+          <div className={collapsed ? styles.collapsed : styles.expanded}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id tempor massa. Duis in
+              suscipit urna. Nunc vel rhoncus lectus. Fusce facilisis mauris at libero mattis porta.
+              Donec elementum orci ac consectetur venenatis. In scelerisque, mauris efficitur volutpat
+              pellentesque, odio mi congue nisl, sit amet accumsan ipsum nibh at odio.
+        </p>
+          </div>
+          <Button compact onClick={() => setCollapsed(!collapsed)}>
+            Show {collapsed ? 'More' : 'Less'}
+          </Button>
+        </div>
       </div>
-    </div>
+      <Divider />
+    </>
   );
 }
 
