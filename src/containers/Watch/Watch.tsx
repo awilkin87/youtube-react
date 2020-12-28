a272d38c968ec9c553935d10e26ff570e78c6da0
Alan Wilkinson <Alan.Wilkinson@q-free.com>
2020-12-28 11:44:25 -0500

    Layout the Watch page Part 14 complete

--- src/containers/Watch/Watch.tsx
+++ src/containers/Watch/Watch.tsx
@@ -4,11 +4,22 @@ import styles from './Watch.module.scss';
 
 export default function Watch() {
   return (
-    <>
-      <RelatedVideos />
-      <div style={{ maxWidth: '80%' }}>
+    <div className={styles.watchGrid}>
+      <div className={styles.video}>
         <Video id='-7fuHEEmEjs' />
       </div>
-    </>
+      <div className={styles.metadata} style={{ width: '100%', height: '100px', background: '#F91112' }}>
+        Metadata
+          </div>
+      <div className={styles.infoBox} style={{ width: '100%', height: '100px', background: '#BD10E0' }}>
+        Video Info Box
+        </div>
+      <div className={styles.comments} style={{ width: '100%', height: '100px', background: '#9013FE' }}>
+        Comments
+        </div>
+      <div className={styles.relatedVideos}>
+        <RelatedVideos />
+      </div>
+    </div>
   );
 }
