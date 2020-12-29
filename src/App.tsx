e21c75c8fb2e90fc494f7fc58fa2bda318196cd2
Alan Wilkinson <Alan.Wilkinson@q-free.com>
2020-12-29 05:19:59 -0500

    Load YouTube Javascript API on initial load

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AppLayout from './components/AppLayout/AppLayout';
 import Home from './containers/Home/Home';
 import Watch from './containers/Watch/Watch';
+import API_KEY from './apiKey';
 
 /** Top-level parent component for the whole app. */
 export default function App() {
+  // Load YouTube API only on initial render
+  useEffect(() => {
+    loadYoutubeApi(API_KEY).then(() => alert('YouTube is now ready!'));
+  }, []);
+
   return (
     <AppLayout>
       <Switch>
@@ -13,4 +20,11 @@ export default function App() {
       </Switch>
     </AppLayout>
   );
+}
+
+async function loadYoutubeApi(apiKey: string) {
+  return window.gapi.client.init({
+    apiKey: apiKey,
+    discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
+  })
 }
---
+++ src/util/google-api-loader.ts
@@ -0,0 +1,45 @@
+/*
+  Not needed anymore. I just put the inital script in index.html. Makes more sense for this
+  application anyway. Left old calls here as an example of wrapping legacy callback in async
+  functions.
+ */
+
+export async function loadYoutubeApi(apiKey: string) {
+  await loadGoogleApi();
+  await loadGoogleClient();
+  await initYoutubeClient(apiKey);
+}
+
+
+// Was having issues loading the client.js alone. The client namespace would not always be
+// available when the callbacks were executed.Loading api.js, then the client, then initializing
+// YouTube API seems to work better.
+
+async function loadGoogleApi() {
+  const script = document.createElement("script");
+  script.src = "https://apis.google.com/js/api.js";
+
+  const promise = new Promise<Event>(resolve => {
+    script.onload = event => { resolve(event) }
+  });
+
+  document.body.appendChild(script);
+
+  return promise;
+}
+
+async function loadGoogleClient() {
+  return new Promise<void>((resolve, reject) => {
+    window.gapi.load('client', {
+      callback: resolve,
+      onerror: reject
+    });
+  });
+}
+
+async function initYoutubeClient(apiKey: string) {
+  return window.gapi.client.init({
+    apiKey: apiKey,
+    discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
+  })
+}
